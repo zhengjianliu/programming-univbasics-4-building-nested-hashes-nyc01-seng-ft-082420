c4220fc59971872f3bbb2eeb3a25a1f1c45bcc08
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 15:15:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,9 @@ def monopoly_with_third_tier
        pieces: 4,
        rent_in_dollars:{
          one_piece_owned:25,
-         
+         two_pieces_owned:50,
+         three_pieces_owned:100,
+         two_pieces_owned:200
        },
        names:{}
      }
