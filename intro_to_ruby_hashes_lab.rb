3583a15eb3fc38dba3c9bb1ffc8a0e946daf11fb
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 15:18:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -73,7 +73,9 @@ def monopoly_with_fourth_tier
          b_and_o_railroad:{
            mortgage_value:400
          },
-         shortline_railroad:{}
+         shortline_railroad:{
+           mortgage_value:800
+         }
        }
      }
   }
