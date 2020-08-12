7e42f7eabab22c1ad8a63056aa2c8fd22f38b782
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 15:10:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  base_hash.newHash[:railroads][:pieces] = 4 
+  newHash[:railroads][:pieces] = 4 
   
   newHash
 end
